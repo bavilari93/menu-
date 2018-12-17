13f71213ea1c1f21de1f2f0ad92cab59cc77f6cf
Bianca A <frank@Bianca-Avila-4.cablecolor.hn>
2018-12-17 16:10:27 -0600

    bigger

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 $(()=>{
-	const nav = document.getElementById('nav');
+	const nav = document.getElementById('nav'), 
+		  menu = document.getElementById('menu');
 	$('.icon').click(()=>{
 		if(nav.className == "top-nav"){
 			nav.className += " responsive";
+			menu.className += " menu";
+			menu.style.display = "flex"
+
 			$(".icon").toggleClass("change");
 		}else{
 			nav.className = "top-nav"
+			menu.style.display ="none";
+			menu.className = "none"
 			$(".icon").removeClass('change');
 		}
 	})
